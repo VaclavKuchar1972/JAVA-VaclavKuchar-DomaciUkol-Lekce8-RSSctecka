f6daeea6b5b6c6f9ef9e94a1e5d7388d9e973cb0
VaclavKuchar1972 <vkcomp@vkcomp.cz>
2023-10-23 20:27:03 +0200

    Initial commit

---
+++ src/main/java/com/entego/rssctecka/RsscteckaApplication.java
@@ -0,0 +1,16 @@
+package com.entego.rssctecka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RsscteckaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RsscteckaApplication.class, args);
+	}
+
+
+
+
+}
